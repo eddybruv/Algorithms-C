9514fd476fb19176a24afe2f94e220ece3761727
eddybruv <edwinajong@gmail.com>
2021-11-16 20:22:16 +0100

    traversing a linked list

---
+++ Linked List/Single /traverse2.c
@@ -0,0 +1,54 @@
+/**
+ * Date: 16-11-2021
+ * by eddybruv
+ * @brief: program to traverse a single linked list.
+ **/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;           //data field
+    struct node *next;  //pointer to next node
+};
+
+void traverse(struct node *head){
+    struct node *current;
+    current = head;
+    int count = 0;
+
+    while (current != NULL){
+        count++; //increase node count
+        printf("%d->", current->data);
+        current = current->next; //sends pointer to next node
+    }
+    printf("NULL\n");
+    printf("Number of nodes: %d", count);
+}
+
+//elements are added to the end of the list but manually
+struct node *createnode(int n){
+    struct node *new_node;
+
+    new_node = (struct node *)malloc(sizeof(struct node));
+
+    new_node->data = n;
+    new_node->next = NULL;
+
+    return new_node;
+}
+
+int main(void){
+    printf("Creating and traversing linked list\n\n\n");
+
+    struct node *head;
+
+    head = createnode(8);
+    head->next = createnode(6);
+    head->next->next = createnode(34);
+    head->next->next->next = createnode(23);
+    head->next->next->next->next = createnode(9);
+    head->next->next->next->next->next = createnode(90);
+
+    traverse(head);
+}
